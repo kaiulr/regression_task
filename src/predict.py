e55ea39e587b062f9b8d7ced7abc87b13353ec92
Fiona <fiona.arora@gmail.com>
2024-09-24 23:05:58 +0530

    First experimentation, R2 = 1.0

---
+++ src/predict.py
@@ -0,0 +1,73 @@
+import pandas as pd
+import numpy as np
+import argparse
+import pickle
+
+# Since predict.py will parse user-inputted arguments, defining functions which take each command line argument as input
+
+def load_model(model_path):
+    with open(model_path, 'rb') as f:
+        regression_model = pickle.load(f)
+    return regression_model
+
+def regression_results(df, output_path):
+    df.to_csv(output_path, index=False)
+
+# Function to save metrics
+def save_metrics(train_error, r2, output_path):
+    regression_metrics = ['Regression Metrics:\n', f'Mean Squared Error (MSE): {train_error}\n', 
+                      f'Root Mean Squared Error (RMSE): {np.sqrt(train_error)}\n', f'R-squared (R²) Score: {r2}\n']
+    with open(output_path, 'w') as f:
+        f.writelines(regression_metrics)
+
+# Function to calculate predictions using the coefficients
+def predict(X, coefficients):
+    X_b = np.c_[np.ones((X.shape[0], 1)), X]  # Add the intercept term
+    return X_b.dot(coefficients)
+
+if __name__ == "__main__":
+    # Setting up argument parser
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--model_path', type=str, required=True, help='Path to the saved model')
+    parser.add_argument('--data_path', type=str, required=True, help='Path to the data (CSV file)')
+    parser.add_argument('--metrics_output_path', type=str, required=True, help='Path to save metrics')
+    parser.add_argument('--predictions_output_path', type=str, required=True, help='Path to save predictions')
+
+    args = parser.parse_args()
+
+    # Load model
+    model_data = load_model(args.model_path)
+    coefficients = model_data['coefficients']
+    feature_names = model_data['features'][1:]  # Skip intercept
+
+    # Load data
+    data = pd.read_csv(args.data_path)
+    
+    # Ensure that the data has the correct features
+    X = data[feature_names].values
+    ytrain = data['FUEL CONSUMPTION'].values
+
+    # Make predictions
+    ypred = predict(X, coefficients)
+
+    # Calculate metrics manually (MSE and R^2)
+    mse = np.mean((ypred - ytrain) ** 2)
+    
+    # Total sum of squares (TSS) and residual sum of squares (RSS)
+    tss = np.sum((ytrain - np.mean(ytrain)) ** 2)
+    rss = np.sum((ypred - ytrain) ** 2)
+    r2 = 1 - (rss / tss)
+
+    # Save predictions to CSV
+    predictions_df = pd.DataFrame({
+        'Actual': ytrain,
+        'Predicted': ypred
+    })
+
+    regression_results(predictions_df, args.predictions_output_path)
+
+    # Save metrics to a text file
+    save_metrics(mse, r2, args.metrics_output_path)
+
+    print(f"Predictions saved to {args.predictions_output_path}")
+    print(f"Metrics saved to {args.metrics_output_path}")
--- src/train_model.py
+++ src/train_model.py
@@ -2,24 +2,24 @@ import numpy as np
 import pandas as pd
 import os
 import matplotlib.pyplot as plt
-
+import pickle
 
 ## Beginning with OLS - Multivariable Regression
 # Based on results from data exploration, using all numerical IVs (Coemissions, Engine Size, Cylinders)
 # Also using encoded Fuel and Transmission values as they are not highly branched.
 
-input_dir = os.path.join(os.path.dirname((os.path.abspath(""))), 'data') 
-df = pd.read_csv(os.path.join(input_dir, 'training_data.csv'))
+input_dir = os.path.join(os.path.dirname((os.path.abspath("")))) 
+df = pd.read_csv(os.path.join(input_dir, 'data', 'training_data.csv'))
 fuel_dummy = [column for column in df.columns if 'FUEL' in column]
 transmission_dummy = [column for column in df.columns if 'TRANSMISSION' in column]
 keep_columns = ['FUEL CONSUMPTION', 'COEMISSIONS','ENGINE SIZE', 'CYLINDERS'] + transmission_dummy + fuel_dummy
-# print(keep_columns)
+
 df = df[keep_columns]
 
 # Converting DF to Numpy array to perform matrix operations
 we=df.to_numpy()
 we=we.astype(np.float64)
-xtrain = we[:, 1:5] # Input matrix of all 5 features
+xtrain = we[:, 1:] # Input matrix of all features
 ytrain = we[:, 0] # Output vector of fuel transmission
 
 ## Creating a function for MSE, which we need for the final model metrics, and to act as a loss function to evaluation model performance
@@ -56,25 +56,50 @@ def Bias_Term(x):
 def Predict(X,b):
     return (np.dot(X,b))
 
-for i in range(2,6):
-    x_train=Bias_Term(xtrain[:,0:i])
-    b=Train(x_train,ytrain)
-    train_predict=Predict(x_train,b)
-    train_error=mse(ytrain,train_predict)
-    print('Training  Error for Multivariable regression using  {} variables is   {}  '.format(i,train_error))
+# for i in range(2,6):
+#     x_train=Bias_Term(xtrain[:,0:i])
+#     b=Train(x_train,ytrain)
+#     train_predict=Predict(x_train,b)
+#     train_error=mse(ytrain,train_predict)
+#     print('Training  Error for Multivariable regression using  {} variables is   {}  '.format(i,train_error))
 
-errors = train_predict - ytrain
-errors_sqr = errors**2
+x_train=Bias_Term(xtrain)
+Beta=Train(x_train,ytrain)
+train_predict=Predict(x_train, Beta)
+train_error=mse(ytrain,train_predict)
+print('Training  Error for Multivariable regression using {} variables is {}  '.format(len(df.columns)-1,train_error))
+print(Beta)
+
+model_data = {
+    'coefficients': Beta, 
+    'features': ['Intercept'] + list(df.columns[1:])
+}
+
+
 
 tss = np.sum((train_predict - np.mean(ytrain)) ** 2)
 rss = np.sum((train_predict - ytrain) ** 2)
 r2 = 1 - (rss / tss)
 print(r2)
 
+regression_metrics = ['Regression Metrics:\n', f'Mean Squared Error (MSE): {train_error}\n', 
+                      f'Root Mean Squared Error (RMSE): {np.sqrt(train_error)}\n', f'R-squared (R²) Score: {r2}\n']
+
+with open(os.path.join(input_dir, 'results', 'metrics.txt'), 'w') as f:
+    f.writelines(regression_metrics)
+
+errors = train_predict - ytrain
+errors_sqr = errors**2
+## Visualizing errors
 # plt.hist(errors_sqr, bins=100)
 # plt.show()
 
-plt.plot(xtrain, ytrain, 'o')
-#add linear regression line to scatterplot 
-plt.plot(xtrain, train_predict, 'x')
-plt.show()
+## Visualizing Predicted vs. Actual Values
+# plt.plot(xtrain, ytrain, 'o')
+# plt.plot(xtrain, train_predict, 'x')
+# plt.show()
+
+with open(os.path.join(input_dir, 'models', 'regression_model1.pkl'), 'wb') as f:
+    pickle.dump(model_data, f)
+
+print("Model saved to 'regression_model1.pkl'")
